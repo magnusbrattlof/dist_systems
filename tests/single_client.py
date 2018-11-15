6f9eca4096abdf63cab10b6401ff139c84d11730
magnusbrattlof <brattlof.magnus@gmail.com>
2018-11-15 09:25:01 +0100

    added some changes

--- tests/single_client.py
+++ tests/single_client.py
@@ -1,28 +1,45 @@
 import requests
 from sys import argv
 
+"""How to use:
+
+$ python tests/single_client.py [action] [item]
+
+where actions are either post, modify or delete
+and item is an integer between 0 and N where N is 
+the total element on your board"""
+
 payload = {}
 
 def main():
+
 	if argv[1] == 'post':
 		post("10.1.0.1", "/board", argv[2])
+
+	elif argv[1] == 'modify':
+		modify(argv[2], argv[3])
+
 	elif argv[1] == 'delete':
 		delete(argv[2])
 
-
 def post(ip, path, items):
+
 	for i in range(int(items)):
 		payload['entry'] = "Message #{}".format(i)
 		requests.post('http://{}{}'.format(ip, path), data=payload)
 
 def delete(items):
+
 	payload['delete'] = 1
-	for i in range(int(items)):
+	for i in items:
 		requests.post('http://10.1.0.1/board/{}/'.format(i), data=payload)
 
+def modify(items, element):
 
-def modify():
-	pass
+	payload['delete'] = 0
+	payload['entry'] = element
+	for i in items:
+		requests.post('http://10.1.0.1/board/{}/'.format(i), data=payload)
 
 
 if __name__ == '__main__':
